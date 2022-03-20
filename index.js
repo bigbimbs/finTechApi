d398f06a49483234f3ef047cbc253710ce22813b
bigbimbs <60890425+bigbimbs@users.noreply.github.com>
2022-03-20 14:48:55 +0100

    @refactor port in index

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const PORT = process.env.LOCALHOSTPORT || "8080";
+const PORT = Process.env.PORT || 3000;
 const router = require("./routers/index.js");
 app.use("/api/v1", router);
 router.get("*", (req, res) => {
