b256e5508d5fb8e64d11d999e695a3ebbf7e9474
bigbimbs <60890425+bigbimbs@users.noreply.github.com>
2022-03-20 13:49:00 +0100

    @fix conflicts with heroku

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const PORT = process.env.LOCALHOSTPORT || "8080";
 const router = require("./routers/index.js");
 app.use("/api/v1", router);
 router.get("*", (req, res) => {
   res.send("URL Resource not found");
 });
 
-app.listen(process.env.LOCALHOSTPORT);
+app.listen(PORT);
